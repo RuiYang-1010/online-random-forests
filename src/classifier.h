12817eac46783034c2551b08b7fab58ecd3a0c0e
Rui Yang <ruiyang1010@hotmail.com>
2021-02-15 17:29:27 +0100

    modify error computing

--- src/classifier.h
+++ src/classifier.h
@@ -17,19 +17,46 @@ virtual vector<Result> trainAndTest(DataSet &dataset_tr, DataSet &dataset_ts) = 
 
 double compError(const vector<Result> &results, const DataSet &dataset) {
         double error = 0.0;
+
         std::cerr<<"CompError Fuction"<<std::endl;
+        double car_numSamples = 0.0;
+        double car_numError = 0.0;
+        double ped_numSamples = 0.0;
+        double ped_numError = 0.0;
+        double cyc_numSamples = 0.0;
+        double cyc_numError = 0.0;
+
         for (int i = 0; i < dataset.m_numSamples; i++) {
-                std::cerr<<"prediction of result:  "<<results[i].prediction<<std::endl;
-                std::cerr<<"confidence of result:  "<<results[i].confidence[0]<<std::endl;
-                std::cerr<<"confidence of result:  "<<results[i].confidence[1]<<std::endl;
-                std::cerr<<"confidence of result:  "<<results[i].confidence[2]<<std::endl;
-                std::cerr<<"label of sample:  "<<dataset.m_samples[i].y<<std::endl;
-                if (results[i].prediction != dataset.m_samples[i].y) {
-                        error++;
+                // std::cerr<<"prediction of result:  "<<results[i].prediction<<std::endl;
+                // std::cerr<<"confidence of result:  "<<results[i].confidence[0]<<std::endl;
+                // std::cerr<<"confidence of result:  "<<results[i].confidence[1]<<std::endl;
+                // std::cerr<<"confidence of result:  "<<results[i].confidence[2]<<std::endl;
+                // std::cerr<<"label of sample:  "<<dataset.m_samples[i].y<<std::endl;
+                // if (results[i].prediction != dataset.m_samples[i].y) {
+                //         error++;
+                // }
+
+                if(dataset.m_samples[i].y == 0){
+                  car_numSamples++;
+                  if(dataset.m_samples[i].y != results[i].prediction){
+                    car_numError++;
+                  }
+                } else if(dataset.m_samples[i].y == 1){
+                  ped_numSamples++;
+                  if(dataset.m_samples[i].y != results[i].prediction){
+                    ped_numError++;
+                  }
+                } else if(dataset.m_samples[i].y == 2){
+                  cyc_numSamples++;
+                  if(dataset.m_samples[i].y != results[i].prediction){
+                    cyc_numError++;
+                  }
                 }
         }
-
-        return error / dataset.m_numSamples;
+        std::cerr<<"ped_numError:  "<<ped_numError<<std::endl;
+        std::cerr<<"ped_numSamples:  "<<ped_numSamples<<std::endl;
+        return ped_numError / ped_numSamples;
+        //return error / dataset.m_numSamples;
 }
 
 void dispErrors(const vector<double> &errors) {
